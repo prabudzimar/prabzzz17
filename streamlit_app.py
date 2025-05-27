33d0626d522811194aee23cd702ba38a8b661598
prabudzimar <dzimarprabu@gmail.com>
2025-05-27 11:24:44 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.title("web milik prabu.")
+st.write("ayo kita belajar coding bersama prabu."
 )
